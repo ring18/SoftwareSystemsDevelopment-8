612da04c3fef3d8963113da36e80fd5b8b90b4e5
ring18 <58505661+ring18@users.noreply.github.com>
2024-01-12 20:12:32 +0900

    Change text above text input

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -4,7 +4,7 @@ function App() {
         <Container>
             <Row>
                 <Col md={{ offset: 3, span: 6 }}>
-                    <p> aaaa </p>
+                    <p> ToDo App by ring18 </p>
                     <TodoListCard />
                 </Col>
             </Row>
